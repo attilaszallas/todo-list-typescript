3f092130b5f08b18967b790b70e92e4edce87660
Attila Szallas <ASzallas@slb.com>
2023-12-06 13:30:13 +0100

    Store and read from cookies

--- src/todo.ts
+++ src/todo.ts
@@ -1,12 +1,43 @@
 type Task = {
+  taskId: number,
   title: string,
   completed: boolean,
 }
+
+let taskId = -1;
 
 const form = document.getElementById('form')! as HTMLElement;
 const input = document.getElementById('input')! as HTMLInputElement;
 const list = document.getElementById('list')! as HTMLUListElement;
 list.setAttribute("style", "list-style-type:none;");
+
+console.log('Cookie reading ...');
+let indexVar: number = 1;
+console.log(document.cookie.valueOf());
+
+let cookieObject = document.cookie.split('; ').reduce((prev, current) => {
+  const [name, ...value] = current.split('=');
+  prev[name] = value.join('=');
+  return prev;
+}, {});
+
+let i : number = 0;
+while(cookieObject[i] != undefined)
+{
+  console.log(`Element #${i} cookieObject: ` + cookieObject[i]);
+
+  taskId = taskId + 1;
+
+  const newTask: Task = {
+    taskId: taskId,
+    title: cookieObject[i],
+    completed: false
+  }
+
+  addListItem(newTask)
+
+  i = i + 1;
+}
 
 document.addEventListener("submit", e => {
   e.preventDefault()
@@ -19,11 +50,16 @@ document.addEventListener("submit", e => {
 
   console.log("input: " + input.value);
 
+  taskId = taskId + 1;
+
   const newTask: Task = {
+    taskId: taskId,
     title: input.value,
     completed: false
   }
 
+  setCookie(`${newTask.taskId}`, `${newTask.title}`)
+  
   addListItem(newTask)
 })
 
@@ -34,15 +70,19 @@ function addListItem(task: Task){
   const div = document.createElement("div")
   div.setAttribute("text-align", "center")
 
+  // Complete Button
   const completeButton = document.createElement("button")
-  completeButton.textContent = "Task Completed"
+  completeButton.textContent = "Completed"
   completeButton.setAttribute("id", "completeButton")
   completeButton.setAttribute("type", "completeButton")
-  
+  completeButton.setAttribute("class", "btn btn-primary")
+
+  // Remove Button
   const removeButton = document.createElement("button") 
-  removeButton.textContent = "Remove Task"
+  removeButton.textContent = "Remove"
   removeButton.setAttribute("id", "removeButton")
   removeButton.setAttribute("type", "removeButton")
+  removeButton.setAttribute("class", "btn btn-primary")
 
   div.append(task.title, completeButton, removeButton)
   item.append(div)
@@ -50,8 +90,47 @@ function addListItem(task: Task){
   console.log("list item appended")
 }
 
+document.addEventListener("completeButton", e => {
+  e.preventDefault()
+
+  console.log("completeButton activated");
+})
+
 document.addEventListener("removeButton", e => {
   e.preventDefault()
 
   console.log("removeButton activated");
 })
+
+/*
+ * General utils for managing cookies in Typescript.
+ */
+function setCookie(name: string, val: string) {
+  const date = new Date();
+  const value = val;
+
+  // Set it expire in 7 days
+  date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+
+  // Set it
+  document.cookie = name+"="+value+"; expires="+date.toUTCString()+"; path=/";
+}
+
+function getCookie(name: string) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  
+  if (parts.length == 2) {
+      return parts.pop().split(";").shift();
+  }
+}
+
+function deleteCookie(name: string) {
+  const date = new Date();
+
+  // Set it expire in -1 days
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+
+  // Set it
+  document.cookie = name+"=; expires="+date.toUTCString()+"; path=/";
+}
--- todo.js
+++ todo.js
@@ -1,7 +1,28 @@
+var taskId = -1;
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 var list = document.getElementById('list');
 list.setAttribute("style", "list-style-type:none;");
+console.log('Cookie reading ...');
+var indexVar = 1;
+console.log(document.cookie.valueOf());
+var cookieObject = document.cookie.split('; ').reduce(function (prev, current) {
+    var _a = current.split('='), name = _a[0], value = _a.slice(1);
+    prev[name] = value.join('=');
+    return prev;
+}, {});
+var i = 0;
+while (cookieObject[i] != undefined) {
+    console.log("Element #".concat(i, " cookieObject: ") + cookieObject[i]);
+    taskId = taskId + 1;
+    var newTask = {
+        taskId: taskId,
+        title: cookieObject[i],
+        completed: false
+    };
+    addListItem(newTask);
+    i = i + 1;
+}
 document.addEventListener("submit", function (e) {
     e.preventDefault();
     if ((input === null || input === void 0 ? void 0 : input.value) == "" || (input === null || input === void 0 ? void 0 : input.value) == null) {
@@ -9,10 +30,13 @@ document.addEventListener("submit", function (e) {
         return;
     }
     console.log("input: " + input.value);
+    taskId = taskId + 1;
     var newTask = {
+        taskId: taskId,
         title: input.value,
         completed: false
     };
+    setCookie("".concat(newTask.taskId), "".concat(newTask.title));
     addListItem(newTask);
 });
 function addListItem(task) {
@@ -21,20 +45,53 @@ function addListItem(task) {
     var label = document.createElement("label");
     var div = document.createElement("div");
     div.setAttribute("text-align", "center");
+    // Complete Button
     var completeButton = document.createElement("button");
-    completeButton.textContent = "Task Completed";
+    completeButton.textContent = "Completed";
     completeButton.setAttribute("id", "completeButton");
     completeButton.setAttribute("type", "completeButton");
+    completeButton.setAttribute("class", "btn btn-primary");
+    // Remove Button
     var removeButton = document.createElement("button");
-    removeButton.textContent = "Remove Task";
+    removeButton.textContent = "Remove";
     removeButton.setAttribute("id", "removeButton");
     removeButton.setAttribute("type", "removeButton");
+    removeButton.setAttribute("class", "btn btn-primary");
     div.append(task.title, completeButton, removeButton);
     item.append(div);
     list === null || list === void 0 ? void 0 : list.append(item);
     console.log("list item appended");
 }
+document.addEventListener("completeButton", function (e) {
+    e.preventDefault();
+    console.log("completeButton activated");
+});
 document.addEventListener("removeButton", function (e) {
     e.preventDefault();
     console.log("removeButton activated");
 });
+/*
+ * General utils for managing cookies in Typescript.
+ */
+function setCookie(name, val) {
+    var date = new Date();
+    var value = val;
+    // Set it expire in 7 days
+    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+    // Set it
+    document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
+}
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) {
+        return parts.pop().split(";").shift();
+    }
+}
+function deleteCookie(name) {
+    var date = new Date();
+    // Set it expire in -1 days
+    date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+    // Set it
+    document.cookie = name + "=; expires=" + date.toUTCString() + "; path=/";
+}
